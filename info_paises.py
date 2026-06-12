fd1a014dd2cd1dc680cde05fc9766a0d71c3f1f9
Gabriel-Hernandez <gabi.57.hernandez@gmail.com>
2026-06-12 12:02:54 -0300

    UPDATE info_paises.py
    Primera estructura, sujeta a cambios

---
+++ info_paises.py
@@ -0,0 +1,152 @@
+"""
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                TRABAJO PRÁCTICO INTEGRADOR|PROGRAMACION|UTN
+                                    ACOSTA BENJAMÍN | HERNÁNDEZ GABRIEL
+                                        GESTIÓN DE DATASET PAISES
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+"""
+
+import csv
+
+# =============================================================================
+# CONSTANTE
+# =============================================================================
+ARCHIVO_CSV = "paises.csv"
+
+
+# =============================================================================
+# FUNCIONES DE VALIDACIÓN
+# =============================================================================
+
+def validar_texto(mensaje):
+    pass
+
+
+def validar_numero(mensaje):
+    pass
+
+
+def validar_rango(mensaje, minimo, maximo):
+    """
+    Pide un número y valida que esté entre minimo y maximo.
+    """
+    pass
+
+
+# ============================================================================
+# FUNCIONES CARGA Y GUARDADO DE CSV
+# =============================================================================
+
+def cargar_paises():
+    pass
+
+
+def guardar_paises(lista_paises):
+    pass
+
+
+# =============================================================================
+# FUNCIONES GREGADO O BUSQUEDA
+# =============================================================================
+
+def agregar_pais(lista_paises):
+    pass
+
+
+def actualizar_pais(lista_paises):
+
+    pass
+
+
+def buscar_pais(lista_paises):
+    pass
+
+
+# =============================================================================
+# FUNCIONES PARA FILTRAR
+# =============================================================================
+
+def filtrar_por_continente(lista_paises):
+    """
+    Pide un continente y muestra todos los países que pertenezcan a él.
+    """
+    pass
+
+
+def filtrar_por_poblacion(lista_paises):
+    """
+    Pide un rango mínimo y máximo de población y muestra los países
+    que estén dentro de ese rango.
+    """
+    pass
+
+
+def filtrar_por_superficie(lista_paises):
+    """
+    Pide un rango mínimo y máximo de superficie y muestra los países
+    que estén dentro de ese rango.
+    """
+    pass
+
+
+# =============================================================================
+# FUNCIONES PARA ORDENAMIENTO
+# =============================================================================
+
+def ordenar_por_nombre_manual(lista_paises, ascendente):
+    """
+    Ordena la lista de países por nombre usando bubble sort u otro
+    """
+    pass
+
+
+def ordenar_por_poblacion(lista_paises, ascendente):
+    """
+    Ordena la lista por población usando  .sort()
+    """
+    pass
+
+
+def ordenar_por_superficie(lista_paises, ascendente):
+    """
+    Ordena la lista por superficie usando .sort() 
+    """
+    pass
+
+
+# =============================================================================
+# FUNCIONES PARA ESTADÍSTICAS
+# =============================================================================
+
+def mostrar_estadisticas(lista_paises):
+
+    pass
+
+
+# =============================================================================
+# FUNCIONES DE MENÚ Y PRESENTACIÓN
+# =============================================================================
+
+def mostrar_menu():
+    """
+    Muestra las opciones 
+    """
+    pass
+
+
+def mostrar_paises(lista_paises):
+    """
+    Muestra tabla.
+    Si lista está vacía, muestra mensaje
+    """
+    pass
+
+
+def main():
+    pass
+
+
+# =============================================================================
+# INICIO
+# =============================================================================
+    main()
